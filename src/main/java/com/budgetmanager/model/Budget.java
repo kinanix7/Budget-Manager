db763e37089c542dac4a32122afa68ee6b79f99d
kinanix7 <kinanix7279x@gmail.com>
2025-05-13 12:58:31 +0100

    ajouter Budget &  Transaction models

--- src/main/java/com/budgetmanager/model/Budget.java
+++ src/main/java/com/budgetmanager/model/Budget.java
@@ -1,5 +1,29 @@
 package com.budgetmanager.model;
 
-public class Budget {
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+import java.math.BigDecimal;
+
+@Entity
+@Table(name = "budgets")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Budget {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private BigDecimal limitAmount;
+    @JoinColumn(name = "category_id", nullable = false, unique = true)
+    private Long category;
+
+    private String startDate;
+    private String endDate;
 }
--- src/main/java/com/budgetmanager/model/Transaction.java
+++ src/main/java/com/budgetmanager/model/Transaction.java
@@ -1,4 +1,38 @@
 package com.budgetmanager.model;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "transactions")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class Transaction {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private BigDecimal amount;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private TransactionType type; // hadi a amine hiya liy kayna fe class enum
+
+    @Column(nullable = false)
+    private LocalDate date;
+
+    private String description;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "category_id", nullable = false)
+    private Category category;
+
 }
